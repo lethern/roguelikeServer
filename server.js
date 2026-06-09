91a6ccb8436f5abcc7d4444a0884a352bf1d3795
lethern <lethern@gmail.com>
2026-06-09 03:05:40 +0200

    fixing websocket port

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const WebSocket = require("ws");
 
-const port = process.env.PORT || 8080;
-const wss = new WebSocket.Server({ port });
+const port = process.env.PORT || 10000;
+const server = require("http").createServer();
+const wss = new WebSocket.Server({ server });
 
 wss.on("connection", ws => {
+	console.log("client connected");
 	ws.on("message", msg => {
 		for (const client of wss.clients) {
 			if (client !== ws && client.readyState === WebSocket.OPEN) {
@@ -11,4 +13,8 @@ wss.on("connection", ws => {
 			}
 		}
 	});
+});
+
+server.listen(port, () => {
+	console.log("running on", port);
 });
