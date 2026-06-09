a0a3713826719db2b862afd7af64df748aae0410
lethern <lethern@gmail.com>
2026-06-09 03:33:26 +0200

    +express

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
+const express = require("express");
 const WebSocket = require("ws");
 
 const port = process.env.PORT || 10000;
 
+const app = express();
+
 // a http server just for Render's port scanner
-const server = require("http").createServer((req, res) => {
-	res.writeHead(200, { "Content-Type": "text/plain" });
-	res.end("Server is running");
+app.get("/", (req, res) => {
+	res.status(200).send("Server is running");
+});
+
+const server = app.listen(port, "0.0.0.0", () => {
+	console.log(`Server is listening on 0.0.0.0:${port}`);
 });
 
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", ws => {
+wss.on("connection", (ws) => {
 	console.log("client connected");
-	ws.on("message", msg => {
+
+	ws.on("message", (msg) => {
 		for (const client of wss.clients) {
 			if (client !== ws && client.readyState === WebSocket.OPEN) {
 				client.send(msg.toString());
 			}
 		}
 	});
-});
 
-server.listen(port, "0.0.0.0", () => {
-	console.log(`Server is listening on 0.0.0.0:${port}`);
+	ws.on("close", () => {
+		console.log("client disconnected");
+	});
 });
